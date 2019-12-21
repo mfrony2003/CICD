b6422ac7c0afc8d20918bfc310dec098f9ed9d84
mfrony2003 <mfrony2003@gmail.com>
2019-12-21 23:21:23 +0600

    initial commit2

--- Test/test.js
+++ Test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../app.js');
 describe('GET /', function() {
- it('respond with hello world', function(done) {
+ it('respond with Hello World', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
+ request(app).get('/').expect('Hello World', done);
  });
 });
--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var app = express();
 
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World');
   });
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
   });
 
+  module.exports = app
